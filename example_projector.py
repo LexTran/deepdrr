afcb4fb6a0593a19d6ea60b20467baf8675c8a95
lex_windows <525048951@qq.com>
2022-05-02 17:32:04 +0800

    updated by lex

--- example_projector.py
+++ example_projector.py
@@ -6,7 +6,7 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
-from rich.logging import RichHandler
+from rich.logging import RichHandler         # 终端美化工具
 from time import time
 
 import deepdrr
@@ -21,24 +21,42 @@ log.addHandler(RichHandler())
 log.setLevel(logging.INFO)
 
 def main():
-    output_dir = test_utils.get_output_dir()
-    data_dir = test_utils.download_sampledata("CTPelvic1K_sample")
-    patient = deepdrr.Volume.from_nifti(
-        data_dir / "dataset6_CLINIC_0001_data.nii.gz", use_thresholding=True
-    )
-    patient.faceup()
+    output_dir = Path.cwd() / "output"                                # 输出文件夹
+    # data_dir = test_utils.download_sampledata("CTPelvic1K_sample")
+    i = 1
+    for data in os.listdir('G:\CS\DL\datasets\CTPelvic1K_dataset6_data'):
+        patient = deepdrr.Volume.from_nifti(
+            # data_dir / "dataset6_CLINIC_0001_data.nii.gz", use_thresholding=True       
+            r'G:\CS\DL\datasets\CTPelvic1K_dataset6_data\\'+data, use_thresholding=True
+        )                                                                       # 指定输入图像，读取CT容积
+        patient.faceup()
 
-    # define the simulated C-arm
-    carm = deepdrr.MobileCArm(patient.center_in_world)
+        # define the simulated C-arm
+        carm = deepdrr.MobileCArm(patient.center_in_world)
+        # deepdrr.vis.show(patient, carm, full=[False, True])
 
-    # project in the AP view
-    with Projector(patient, carm=carm) as projector:
-        carm.move_to(alpha=0, beta=-15)
-        image = projector()
+        # project in the AP view
+        with Projector(patient, 
+            carm=carm,
+            photon_count=100000,
+            add_noise=True,
+            # scatter_num=10,
+            threads=8,
+            neglog=True,
+        ) as projector:
+            #carm.move_to(alpha=0, beta=-15)
+            image = projector()
 
-    path = output_dir / "example_projector.png"
-    image_utils.save(path, image)
-    log.info(f"saved example projection image to {path.absolute()}")
+        path = output_dir / f'example_projector_{i}.png'
+        image_utils.save(path, image)
+        i += 1
+        log.info(f"saved example projection image to {path.absolute()}")
 
 if __name__ == "__main__":
+# if(os.system("cl.exe")):
+    os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
+    os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include"
+    os.environ['PATH'] += ';'+r"D:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt"
+    if(os.system("cl.exe")):
+        raise RuntimeError("cl.exe still not found, path probably incorrect")
     main()
--- geometry_testing.py
+++ geometry_testing.py
@@ -1,16 +1,21 @@
 import deepdrr
 from deepdrr import geo
 from deepdrr.utils import test_utils
+from deepdrr.utils import image_utils
 import numpy as np
 import logging
+import os
+from PIL import Image
+import pdb
 
 log = logging.getLogger(__name__)
 
 def test_scatter_geometry():
-    file_path = test_utils.download_sampledata("CT-chest")
-    volume = deepdrr.Volume.from_nrrd(file_path)
+    file_path = 'G:\CS\DL\datasets\CTPelvic1K_dataset6_data\dataset6_CLINIC_0026_data.nii.gz'
+    volume = deepdrr.Volume.from_nifti(file_path, use_thresholding=True)
+    volume.faceup()
 
-    carm = deepdrr.MobileCArm(isocenter=volume.center_in_world)
+    carm = deepdrr.MobileCArm(volume.center_in_world)
 
     print(f"volume center in world: {volume.center_in_world}")
     print(f"volume spacing: {volume.spacing}")
@@ -19,22 +24,25 @@ def test_scatter_geometry():
     with deepdrr.Projector(
         volume=volume,
         carm=carm,
-        step=0.1,
-        mode="linear",
-        max_block_index=200,
-        spectrum="90KV_AL40",
+        max_block_index=1024,
         photon_count=100000,
-        scatter_num=10e7,
+        add_noise=True,
+        # scatter_num=10e7,
         threads=8,
         neglog=True,
     ) as projector:
+        # carm.move_to(alpha=0, beta=15, degrees=True)
         image = projector.project()
 
-    image = (image * 255).astype(np.uint8)
-    #Image.fromarray(image).save("output/test_multivolume.png")
+    image_utils.save("output/test_multivolume.png", image)
     
     output_dir = test_utils.get_output_dir()
 
 if __name__ == "__main__":
+    os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx64\x64"
+    os.environ['PATH'] += ';'+r"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include"
+    os.environ['PATH'] += ';'+r"D:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt"
+    if(os.system("cl.exe")):
+        raise RuntimeError("cl.exe still not found, path probably incorrect")
     logging.getLogger("deepdrr").setLevel(logging.DEBUG)
     test_scatter_geometry()
